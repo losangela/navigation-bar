136d86af336ba2e833b1f52229b79dea2507d3a4
Angela Choi <eunsol.angela.choi@gmail.com>
2019-07-11 16:23:57 -0700

    final updates on icons buttons and CSS

--- client/components/NavMain.jsx
+++ client/components/NavMain.jsx
@@ -33,7 +33,7 @@ class NavMain extends React.Component {
           <span className="nav-top-container-logo">
             <div className="nav-top-container-btn-right">
               <button className="nav-top-button">
-                <img src="nav_logo.png" height="40px"></img>
+              <img src="https://s3-us-west-1.amazonaws.com/petfinder-nav/nav_logo.png" height="40px"></img>
 
               </button>
               
@@ -66,77 +66,12 @@ class NavMain extends React.Component {
           <span className="nav-top-container-search">
             <div className="nav-top-container-btn-right left-border">
               <button className="nav-top-button padded2" onClick={this.handleSearchSwitch}>
-              🔍
+                <div className="nav-search-icon"></div>
               </button>
             </div>
           </span>
-
-
-
         </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-        // <div id="nav-bar-main">
-        //   <div className="nav-button fake">.</div>
-        //   <div className="nav-button">Pet Finder</div>
-
-        //   <div className="nav-button">
-        //     <button className="nav-bar-main-button" onClick={() => this.props.handleNavFormBar('findPet')}>FIND A PET</button>
-        //   </div>
-        //   <div className="nav-button">
-        //     <button className="nav-bar-main-button" onClick={() => this.props.handleNavFormBar('breeds')}>BREEDS</button>
-        //   </div>
-        //   <div className="nav-button">
-        //     <button className="nav-bar-main-button" onClick={() => this.props.handleNavFormBar('resources')}>RESOURCES</button>
-        //   </div>
-        // <div className="nav-button search" onClick = {this.handleSearchSwitch}>?</div>
-        // </div> 
-          
-        // <span className="nav-button">
-            
-        //       <span className="filler">
-        //       <button className="nav-bar-main-button">
-        //         <table align="center" id="nav-find-search-type-table"><tr><td>Pet Finder </td><td>  </td></tr></table>
-        //         </button>
-        //       </span>
-        //   </span>
-
-        //   <span className="nav-button">
-        //     <button className="nav-bar-main-button" onClick={() => this.props.handleNavFormBar('findPet')}>
-        //       <span className="filler">
-        //         <table align="center" id="nav-find-search-type-table"><tr><td>FIND A PET </td><td> <img src="https://www.iconsdb.com/icons/preview/white/cat-xxl.png" height="15px"></img> </td></tr></table>
-        //       </span>
-        //     </button>
-        //   </span>
-        //   <span className="nav-button">
-        //     <button className="nav-bar-main-button" onClick={() => this.props.handleNavFormBar('breeds')}>
-        //       <span className="filler">
-        //         <table align="center" id="nav-find-search-type-table"><tr><td>BREEDS </td><td> <img src="https://www.iconsdb.com/icons/preview/white/cat-xxl.png" height="15px"></img> </td></tr></table>
-        //       </span>
-        //     </button>
-        //   </span>
-        //   <span className="nav-button">
-        //     <button className="nav-bar-main-button" onClick={() => this.props.handleNavFormBar('resources')}>
-        //       <span className="filler">
-        //         <table align="center" id="nav-find-search-type-table"><tr><td>RESOURCES </td><td> <img src="https://www.iconsdb.com/icons/preview/white/cat-xxl.png" height="15px"></img> </td></tr></table>
-        //       </span>
-        //     </button>
-        //   </span>
-          
-
-          
-          
       )
     } else if (this.state.search === true) {
       return (
@@ -144,7 +79,7 @@ class NavMain extends React.Component {
           <span className="nav-top-container-logo">
             <div className="nav-top-container-btn-right">
               <button className="nav-top-button">
-                <img src="nav_logo3.png" height="40px"></img>
+              <img src="https://s3-us-west-1.amazonaws.com/petfinder-nav/nav_logo2.png" height="40px"></img>
               </button>
             </div>
           </span>
@@ -163,12 +98,12 @@ class NavMain extends React.Component {
           <span className="nav-top-container-search">
             <div className="nav-top-container-btn left-border">
               <button className="nav-top-button padded2" onClick={() => alert("Searching: " + this.state.articleSearch)}>
-              🔍
+                <div className="nav-searching-icon"></div>
               </button>
             </div>
             <div className="nav-top-container-btn-right left-border">
-              <button className="nav-top-button padded2 white large" onClick={this.handleSearchSwitch}>
-              &times;
+              <button className="nav-top-button padded2 white large navClose" onClick={this.handleSearchSwitch}>
+              
               </button>
             </div>
           </span>
